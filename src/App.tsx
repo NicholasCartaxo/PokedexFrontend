2a28ab21b924f94ed5fb854b209172ade1237407
Ana <ana.ceci7373@gmail.com>
2025-06-06 17:35:19 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState, type JSX } from 'react'
+import './App.css'
+
+
+function PokeCard() : JSX.Element{
+  
+
+  return(
+    <div>
+    </div>
+  );
+}
+
+
+
+function App() {
+  return <> tem nada fi</>
+}
+
+export default App
