49b0dd9585ca3214fe9a40a3eaac819076973905
Nicholas Cartaxo <nicholas.cartaxo.de.souza.rangel.alves@ccc.ufcg.edu.br>
2025-06-09 17:17:59 -0300

    added shoreline and defined main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
-import { useState, type JSX } from 'react'
+import { startTransition, useEffect, useState, type JSX, type SetStateAction } from 'react'
 import './App.css'
+import { Center, Collection, CollectionRow, Filter, FilterListSkeleton, Pagination, Search } from '@vtex/shoreline'
 
 
 function PokeCard() : JSX.Element{
-  
+  return <></>; 
+}
 
-  return(
-    <div>
-    </div>
-  );
+function FilterSearch({label} : {label : string}) : JSX.Element{
+
+  return <Filter label={label}></Filter>
 }
 
 
+function Pokedex() : JSX.Element{
+  return(
+    <Collection>
+      <CollectionRow>
+        <Search></Search>
+        <Pagination page={1} total={10}></Pagination>
+      </CollectionRow>
+
+      <CollectionRow>
+        <FilterSearch label="type"></FilterSearch>
+      </CollectionRow>
+    
+    </Collection>
+  );
+}
 
 function App() {
-  return <> tem nada fi</>
+  return (
+    <Center>
+      <Pokedex />
+    </Center>
+  )
 }
 
 export default App
