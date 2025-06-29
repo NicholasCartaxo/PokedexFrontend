919fb48f2c13667fba610a7198e32435bf0dc76f
Nicholas Cartaxo <nicholas.cartaxo.de.souza.rangel.alves@ccc.ufcg.edu.br>
2025-06-29 06:30:58 -0300

    fixes for gh deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@vtex/shoreline/css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Pokedex from './components/Pokedex';
 import Layout from './components/Layout';
 import PokePage from './components/PokePage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Pokedex />} />
           <Route path="pokemon/:pokeId" element={<PokePage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
